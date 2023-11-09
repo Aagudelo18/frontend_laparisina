7bd24d9f241b71c6c5b2cc87718a00b7cd561f89
Isaaacmz79 <medebes20000@gmail.com>
2023-11-09 09:59:28 -0500

    no envía id del usuario

--- src/app/demo/components/pages/usuarios/usuarios.component.ts
+++ src/app/demo/components/pages/usuarios/usuarios.component.ts
@@ -1,25 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { UsuarioService } from './usuarios.service';
+import { RolesService } from './usuarios.service';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   templateUrl: './usuarios.component.html',
-  providers: [UsuarioService]
+  providers: [UsuarioService, RolesService]
 })
 export class UsuariosComponent implements OnInit {
   usuarios: any[] = [];
   selectedUsuarios: any[] = [];
+  roles: any[] = []; //Se declara una lista para roles
+  rolesList: string[] = []; // Declaramos la propiedad rolesList
   usuarioDialog: boolean = false;
   formularioUsuario: FormGroup;
 
-  constructor(private usuarioService: UsuarioService, private fb: FormBuilder) {
+  hideUsuarioDialog() {
+    this.usuarioDialog = false;
+  }
+
+
+  constructor(private usuarioService: UsuarioService, private rolesService: RolesService, private fb: FormBuilder) {
     this.formularioUsuario = fb.group({
       correo_electronico: ['', [Validators.required, Validators.email]],
       contrasena_usuario: ['', [Validators.required, Validators.minLength(6)]],
-      rol_usuario: ['', Validators.required],
-      estado_usuario: ['', Validators.required],
+      rol_usuario: ['', [Validators.required]],
+      estado_usuario: [true, Validators.required], // Establece el valor predeterminado a "Activo"
     });
+    
   }
 
   ngOnInit() {
@@ -29,20 +38,49 @@ export class UsuariosComponent implements OnInit {
         console.log(this.usuarios);
       }
     });
+
+    this.rolesService.getRoles().subscribe((data: any[]) => {
+      this.roles = data;
+      console.log(data);
+
+      // Crea la lista de roles
+      const rolesList = this.roles.map((rol: any) => rol.nombre_rol);
+      
+      // Asigna la lista de roles a la propiedad roles
+      this.rolesList = rolesList;
+      console.log(rolesList);
+    });
   }
+
 
   openNewUsuarioDialog() {
     this.usuarioDialog = true;
   }
 
+  estadoOptions = [
+    { label: 'Activo', value: true },
+    { label: 'Inactivo', value: false }
+  ];
+
+
   crearUsuario() {
     if (this.formularioUsuario.valid) {
       const nuevoUsuario = this.formularioUsuario.value;
+
+      // Obtiene el nombre del rol seleccionado
+      const rolSeleccionado = this.rolesList[this.formularioUsuario.get('rol_usuario').value];
+
+      // Asigna el nombre del rol seleccionado al objeto nuevoUsuario
+      nuevoUsuario.rol_usuario = rolSeleccionado;
+
       this.usuarioService.createUsuario(nuevoUsuario).subscribe((response: any) => {
         console.log('Usuario creado con éxito:', response);
         this.usuarioDialog = false;
-        // Puedes agregar lógica adicional aquí, como actualizar la lista de usuarios.
       });
     }
   }
+  
+  
+
+
 }
--- src/app/demo/components/pages/usuarios/usuarios.service.ts
+++ src/app/demo/components/pages/usuarios/usuarios.service.ts
@@ -1,39 +1,54 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UsuarioService {
-  private apiUrl = 'http://localhost:3000/api/usuarios';
+export class RolesService {
+
+  private apiUrl = 'http://localhost:3000/api';
+
+  constructor(private http: HttpClient) {}
+
+  //Método para obtener la lista de roles desde la API
+  getRoles(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/roles`);
+  }
+}
+
+@Injectable({
+  providedIn: 'root'
+}) export class UsuarioService {
+  private apiUrl = 'http://localhost:3000/api';
 
   constructor(private http: HttpClient) { }
 
   //Traer todos los usuarios
   getUsuarios() {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<any[]>(`${this.apiUrl}/usuarios`);
   }
 
   //Crear usuario
   createUsuario(usuarioData: any) {
-    return this.http.post(this.apiUrl, usuarioData);
+    return this.http.post(`${this.apiUrl}/usuarios`, usuarioData);
   }
 
   //Obtener un usuario por ID
   getUsuarioPorId(id: string) {
-    const url = `${this.apiUrl}/${id}`;
+    const url = `${this.apiUrl}/usuarios/${id}`;
     return this.http.get<any>(url);
   }
 
   //Actualizar un usuario
   updateUsuario(id: string, usuarioData: any) {
-    const url = `${this.apiUrl}/${id}`;
+    const url = `${this.apiUrl}/usuarios/${id}`;
     return this.http.put(url, usuarioData);
   }
 
   //Eliminar un usuario
   deleteUsuario(id: string) {
-    const url = `${this.apiUrl}/${id}`;
+    const url = `${this.apiUrl}/usuarios/${id}`;
     return this.http.delete(url);
   }
   
