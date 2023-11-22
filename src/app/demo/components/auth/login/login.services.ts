e2e4a7b9464f8271373df616ebb0f4438fa4a4c1
Isaaacmz79 <medebes20000@gmail.com>
2023-11-22 09:54:12 -0500

    login con render

--- src/app/demo/components/auth/login/login.services.ts
+++ src/app/demo/components/auth/login/login.services.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable({
     providedIn: 'root'
 }) export class LoginService {
-    private apiUrl = 'http://localhost:3000/api';
+    private apiUrl = 'https://api-parisina-2tpy.onrender.com/api';
 
     constructor(private http: HttpClient) { }
 
