d05edb21088c123405e4d165d998e03942a68da7
Isaaacmz79 <medebes20000@gmail.com>
2024-03-19 22:11:50 -0500

    pagina no encontrada

--- src/app/demo/components/pages/no_permisos/no_permiso.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-no_permiso',
-    templateUrl: './no_permiso.component.html',
-})
-export class No_permisoComponent { }
